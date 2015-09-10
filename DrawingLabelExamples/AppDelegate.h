f0c8dee17092840a86fbe2e06a1068c7ac654926
Selim Bakdemir <selim.bakdemir@gmail.com>
2015-09-10 20:03:09 +0300

    Initial Commit

---
+++ DrawingLabelExamples/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawingLabelExamples
+//
+//  Created by Selim on 10.09.15.
+//  Copyright (c) 2015 Selim Bakdemir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DrawingLabelExamples/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrawingLabelExamples
+//
+//  Created by Selim on 10.09.15.
+//  Copyright (c) 2015 Selim Bakdemir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
